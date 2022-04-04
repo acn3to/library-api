609b0acff7935d762147e80814e8d8a1d7ea9ae3
Arnaldo Neto <costanetoow@gmail.com>
2022-04-04 20:39:51 -0300

    Add README.md and first route POST

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const books = [
-  { id: "1", title: "Lord of the Rings" },
-  { id: "2", title: "The Hobbit" },
+  { id: 1, title: "Lord of the Rings" },
+  { id: 2, title: "The Hobbit" },
 ];
 
 app.get("/", (_req, res) => {
@@ -13,6 +15,11 @@ app.get("/", (_req, res) => {
 
 app.get("/books", (_req, res) => {
   res.status(200).json(books);
+});
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  res.status(201).send("Books added successfully!");
 });
 
 export default app;
