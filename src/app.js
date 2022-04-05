84c2ccec04c99e8e14eab8f57aae02fa3614374f
Arnaldo Neto <costanetoow@gmail.com>
2022-04-04 21:01:33 -0300

    First route PUT

--- src/app.js
+++ src/app.js
@@ -17,9 +17,24 @@ app.get("/books", (_req, res) => {
   res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+  let index = searchBook(req.params.id);
+  res.json(books[index]);
+});
+
 app.post("/books", (req, res) => {
   books.push(req.body);
   res.status(201).send("Books added successfully!");
 });
+
+app.put("/books/:id", (req, res) => {
+  let index = searchBook(req.params.id);
+  books[index].title = req.body.title;
+  res.json(books);
+});
+
+function searchBook(id) {
+  return books.findIndex((book) => book.id == id);
+}
 
 export default app;
